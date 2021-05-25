c3325b8c71a5c391ea710df96c70f3858ee7d353
Mayue <mayue8373@163.com>
2021-05-25 21:38:31 +0800

    gRpc 2

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,8 +1,9 @@
 ﻿using Grpc.Core;
+using Grpc.Core.Interceptors;
 using Grpc.Net.Client;
+using Grpc.Net.Client.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace GrpcClient
@@ -19,66 +20,110 @@ namespace GrpcClient
                 logging.SetMinimumLevel(LogLevel.Debug);
             });
 
-            using var channel = GrpcChannel.ForAddress("https://localhost:5001",
-                new GrpcChannelOptions { LoggerFactory = loggerFactory });
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-            var client = new Greeter.GreeterClient(channel);
+            //var defaultMethodConfig = new MethodConfig
+            //{
+            //    Names = { MethodName.Default },
+            //    RetryPolicy = new RetryPolicy
+            //    {
+            //        MaxAttempts = 5,
+            //        InitialBackoff = TimeSpan.FromSeconds(1),
+            //        MaxBackoff = TimeSpan.FromSeconds(5),
+            //        BackoffMultiplier = 1.5,
+            //        RetryableStatusCodes = { StatusCode.Unavailable }
+            //    }
+            //};
+
+            //var channel = GrpcChannel.ForAddress("http://localhost:5000",
+            //    new GrpcChannelOptions
+            //    {
+            //        LoggerFactory = loggerFactory,
+            //        ServiceConfig = new ServiceConfig
+            //        {
+            //            MethodConfigs = { defaultMethodConfig }
+            //        }
+            //    });
+
+            var defaultMethodConfig = new MethodConfig
+            {
+                Names = { MethodName.Default },
+                HedgingPolicy = new HedgingPolicy
+                {
+                    MaxAttempts = 5,
+                    NonFatalStatusCodes = { StatusCode.Unavailable }
+                }
+            };
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5000", new GrpcChannelOptions
+            {
+                LoggerFactory = loggerFactory,
+                ServiceConfig = new ServiceConfig { MethodConfigs = { defaultMethodConfig } }
+            });
+
+            //拦截器
+            var invoker = channel.Intercept(new ClientLoggerInterceptor());
+
+            var client = new Greeter.GreeterClient(invoker);
             var reply = await client.SayHelloAsync(
-              new HelloRequest { Name = "GreeterClient" });
+              new HelloRequest { Name = "GreeterClient" },deadline: DateTime.UtcNow.AddSeconds(5));
             Console.WriteLine("Greeting: " + reply.Message);
 
-            var token = new CancellationTokenSource(TimeSpan.FromSeconds(5));
-            using var replyStreamServer = client.StreamingServer(
-                new HelloRequest { Name = "StreamingServer" },
-                cancellationToken: token.Token);
-            try
-            {
-                await foreach (var item in replyStreamServer.ResponseStream.ReadAllAsync(token.Token))
-                {
-                    Console.WriteLine(item.Message);
-                }
-            }
-            catch (RpcException exc)
-            {
-                Console.WriteLine(exc.Message);
-            }
+            //var token = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+            //using var replyStreamServer = client.StreamingServer(
+            //    new HelloRequest { Name = "StreamingServer" },
+            //    cancellationToken: token.Token);
+            //try
+            //{
+            //    await foreach (var item in replyStreamServer.ResponseStream.ReadAllAsync(token.Token))
+            //    {
+            //        Console.WriteLine(item.Message);
+            //    }
+            //}
+            //catch (RpcException exc)
+            //{
+            //    Console.WriteLine(exc.Message);
+            //}
 
-            using var replyStreamClient = client.StreamingClient();
+            //using var replyStreamClient = client.StreamingClient();
 
-            foreach (var name in new[] { "Stacking", "Client", "Stream" })
-            {
-                await replyStreamClient.RequestStream.WriteAsync(new HelloRequest
-                {
-                    Name = name
-                });
-            }
+            //foreach (var name in new[] { "Stacking", "Client", "Stream" })
+            //{
+            //    await replyStreamClient.RequestStream.WriteAsync(new HelloRequest
+            //    {
+            //        Name = name
+            //    });
+            //}
 
-            await replyStreamClient.RequestStream.CompleteAsync();
-            var response = await replyStreamClient.ResponseAsync;
+            //await replyStreamClient.RequestStream.CompleteAsync();
+            //var response = await replyStreamClient.ResponseAsync;
 
-            Console.WriteLine(response.Count);
-            Console.WriteLine(response.Message);
+            //Console.WriteLine(response.Count);
+            //Console.WriteLine(response.Message);
 
-            using var replyStreamWays = client.StreamingWays();
-            for (int i = 0; i < 5; i++)
-            {
-                await replyStreamWays.RequestStream.WriteAsync(new HelloRequest
-                {
-                    Name = "StreamWaysName " + i,
-                });
-            }
 
-            while (await replyStreamWays.ResponseStream.MoveNext())
-            {
-                try
-                {
-                    Console.WriteLine($"Response Return:{replyStreamWays.ResponseStream.Current.Message}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
+            //using var replyStreamWays = client.StreamingWays();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    await replyStreamWays.RequestStream.WriteAsync(new HelloRequest
+            //    {
+            //        Name = "StreamWaysName " + i,
+            //    });
+            //}
+
+            //while (await replyStreamWays.ResponseStream.MoveNext())
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine($"Response Return:{replyStreamWays.ResponseStream.Current.Message}");
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+            //}
+
+            //await replyStreamWays.RequestStream.CompleteAsync();
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
